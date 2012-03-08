6fcd51ebbbaa5f1fcc2103214cf5aa967d942e7b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-03-08 10:51:34 +0100

    integrate the tranformer broadcaster

---
+++ models/orogen/transformer.rb
@@ -0,0 +1,13 @@
+class Orocos::RobyPlugin::Transformer::Task
+    attr_reader :last_update
+
+    poll do
+        update_time, info = *Roby.app.orocos_engine.transformer_configuration_state
+        if last_update != update_time
+            orogen_task.setConfiguration(info)
+            Typelib.copy(Orocos.transformer.configuration_state, info)
+            @last_update = update_time
+        end
+    end
+end
+
