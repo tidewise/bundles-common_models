7001b028dbab0e67321b353a38281b32253571cb
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-03-28 22:15:39 +0200

    define the joystick and controldev base services so that we can select the input method independently of the device

--- models/orogen/controldev.rb
+++ models/orogen/controldev.rb
@@ -1,21 +1,36 @@
 load_system_model 'blueprints/control'
 
-device_type 'Joystick' do
+data_service_type 'Joystick' do
     provides Srv::Motion2DController
 end
+device_type 'Joystick' do
+    provides Srv::Joystick
+end
 device_type 'RemoteJoystick' do
-    provides Srv::Motion2DController
+    provides Srv::Joystick
 end
 
 Cmp::ControlLoop.declare 'FourWheel', 'controldev/FourWheelCommand'
 
-device_type 'RemoteSliderbox' do
+data_service_type 'Sliderbox' do
     provides Srv::FourWheelController
+end
+
+device_type 'Sliderbox' do
+    provides Srv::Sliderbox
+end
+
+device_type 'RemoteSliderbox' do
+    provides Srv::Sliderbox
 end
 
 class Controldev::Remote
     driver_for Dev::RemoteJoystick, :as => 'joystick'
     driver_for Dev::RemoteSliderbox, :as => 'sliderbox'
+end
+
+class Controldev::SliderboxTask
+    driver_for Dev::Sliderbox
 end
 
 class Controldev::JoystickTask
