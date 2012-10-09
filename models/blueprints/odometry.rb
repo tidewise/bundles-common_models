030797cf3d58fbb561c4dc391f88e12a48d3a4b6
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-10-09 09:55:11 +0200

    fix odometry-related loading mess

--- models/blueprints/odometry.rb
+++ models/blueprints/odometry.rb
@@ -1,3 +1,17 @@
+load_system_model 'blueprints/pose'
+import_types_from 'odometry'
+
+data_service_type 'Odometry' do
+    provides Srv::RelativePose
+    provides Srv::PoseDelta
+end
+
+# This data service provides contact point of the robot 
+# with its environment.
+data_service_type 'BodyContactState' do
+    output_port 'contact_samples', '/odometry/BodyContactState'
+end
+
 
 composition 'Odometry' do
     add Srv::Orientation, :as => 'imu'
