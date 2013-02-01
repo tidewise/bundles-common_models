ae88dd3a1cd2f9e666f2e1ab0f6848f33844da21
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-02-01 17:38:22 +0100

    add support for iodrivers_base
    It defines a generic Base::RawIODeviceSrv which is used for any
    task that can interface using the RawPacket data type

---
+++ models/orogen/iodrivers_base.rb
@@ -0,0 +1,13 @@
+module Base
+    data_service_type 'RawIODeviceSrv' do
+        input_port 'raw_in', '/iodrivers_base/RawPacket'
+        output_port 'raw_out', '/iodrivers_base/RawPacket'
+    end
+end
+
+class IodriversBase::Task
+    provides Base::RawIODeviceSrv, :as => 'raw_io',
+        'raw_out' => 'io_raw_out',
+        'raw_in' => 'io_raw_in'
+end
+
