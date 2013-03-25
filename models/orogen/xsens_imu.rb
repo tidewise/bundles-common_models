dedb7adbd607bd5c8c7a89cbf07e9e849ddc9207
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-03-25 21:42:13 +0100

    fix frame name in xsens_imu::Task declarations
    The frame got renamed from 'xsens' to 'imu'

--- models/orogen/xsens_imu.rb
+++ models/orogen/xsens_imu.rb
@@ -9,8 +9,8 @@ class XsensImu::Task
     # Additional information to allow for the transformer's automatic
     # configuration
     transformer do
-        associate_frame_to_ports "xsens", "calibrated_sensors"
-        transform_output "orientation_samples", "xsens" => "world"
+        associate_frame_to_ports "imu", "calibrated_sensors"
+        transform_output "orientation_samples", "imu" => "world"
     end
     
     driver_for Dev::Sensors::XsensAHRS, :as => 'driver'
