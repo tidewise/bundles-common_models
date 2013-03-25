66f4b735c8d3eca0ca3b76c3e0c4fbef5ab8e427
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-03-25 17:47:20 +0100

    fix the transformer broadcaster support

--- models/orogen/transformer.rb
+++ models/orogen/transformer.rb
@@ -2,10 +2,9 @@ class Transformer::Task
     attr_reader :last_update
 
     poll do
-        update_time, info = *Roby.app.orocos_engine.transformer_configuration_state
+        update_time, info = *plan.transformer_configuration_state
         if last_update != update_time
-            orogen_task.setConfiguration(info)
-            Typelib.copy(Orocos.transformer.configuration_state, info)
+            orocos_task.setConfiguration(info)
             @last_update = update_time
         end
     end
