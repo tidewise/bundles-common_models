1b180acaa703d7e2aabe02b6ad43a6013c9f4cb6
Felix Rehrmann <felix.rehrmann@dfki.de>
2013-04-04 16:29:20 +0200

    added definitions for laser_filter::Task
    - Task provides a LaserRangeFinderSrv given in frame 'laser'
    - defines a compositon FilteredLaserRangeFinder

---
+++ models/orogen/laser_filter.rb
@@ -0,0 +1,26 @@
+require 'models/blueprints/sensors'
+require 'models/blueprints/devices'
+
+class LaserFilter::Task
+    provides Base::LaserRangeFinderSrv, :as => 'filtered_scans'
+
+    transformer do
+        associate_frame_to_ports 'laser', 'filtered_scans'
+    end
+
+end
+
+module Base
+
+    class FilteredLaserRangeFinder < Syskit::Composition
+        add Base::LaserRangeFinderSrv, :as => 'source'
+        add LaserFilter::Task, :as => 'filter'
+
+        source_child.scans_port.connect_to filter_child.scan_samples_port
+
+        export filter_child.filtered_scans_port
+        provides Base::LaserRangeFinderSrv, :as => 'filtered_scans'
+    end
+
+end
+
