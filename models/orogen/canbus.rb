075d42d2909dad28ecd7b39824d158c4315d1de5
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-04-12 12:19:07 +0200

    can: style fix

--- models/orogen/canbus.rb
+++ models/orogen/canbus.rb
@@ -1,17 +1,15 @@
-module Dev
-    module Bus
-        com_bus_type 'CAN', :message_type => '/canbus/Message' do
-            extend_attached_device_configuration do
-                dsl_attribute :can_id do |id, mask|
-                    mask ||= id
-                    id   = Integer(id)
-                    mask = Integer(mask)
-                    if (id & mask) != id
-                        raise ArgumentError, "wrong id/mask combination: some bits in the ID are not set in the mask, and therefore the filter will never match"
-                    end
-                    [id, mask]
-                end
+require 'models/blueprints/devices'
+
+Dev::Bus.com_bus_type 'CAN', :message_type => '/canbus/Message' do
+    extend_attached_device_configuration do
+        dsl_attribute :can_id do |id, mask|
+            mask ||= id
+            id   = Integer(id)
+            mask = Integer(mask)
+            if (id & mask) != id
+                raise ArgumentError, "wrong id/mask combination: some bits in the ID are not set in the mask, and therefore the filter will never match"
             end
+            [id, mask]
         end
     end
 end
