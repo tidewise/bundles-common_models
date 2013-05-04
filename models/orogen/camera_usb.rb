faa6a34f20b3d68ea7165bca73188c0f6cf55fd0
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-05-04 20:11:17 +0200

    integrate camera_usb::Task

---
+++ models/orogen/camera_usb.rb
@@ -0,0 +1,14 @@
+require 'models/blueprints/devices'
+
+class CameraUsb::Task
+    driver_for Dev::Sensors::Cameras::USB, :as => 'driver'
+    
+    def configure
+        super
+        if p = robot_device.period
+            orocos_task.fps = (1.0 / p).round
+        end
+    end
+end
+
+
