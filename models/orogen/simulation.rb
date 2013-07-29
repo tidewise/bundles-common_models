c17204ee5dd7bbb7310c10e3e114f87d6431b17b
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2013-07-29 17:04:23 +0200

    More cleanup on simulation plugin

--- models/orogen/simulation.rb
+++ models/orogen/simulation.rb
@@ -46,32 +46,64 @@ class Simulation::Actuators
     driver_for Dev::Simulation::Actuator, :as => "driver"
 end
 
-class Simulated
+class Syskit::Actions::Profile
+    #
+    # Instead of doing
+    #   define 'dynamixel', Model
+    #
+    # Do
+    #
+    #   define_simulated_device 'dynamixel', Dev::Simulation::Sonar
+    #
+    def define_simulated_device(name, model, options = Hash.new)
+        robot.device model, :as => name
+        define name, Simulated::getSensor(model.name,options) #composition_from_device_type(model)#.use(mars_dev)
+    end
+end
+
+module Simulated
+    
+
     class Servo < Syskit::Composition
         add Simulation::Mars, :as => "mars"
-        add Simulation::MarsServo, :as => "servo"
+        add Simulation::MarsServo, :as => "task"
     end
+    
     class Actuator < Syskit::Composition
         add Simulation::Mars, :as => "mars"
-        add Simulation::Actuators, :as => "actuator"
+        add Simulation::Actuators, :as => "task"
         
-        actuator_child.each_output_port do |port|
+        task_child.each_output_port do |port|
             export port
         end
-        actuator_child.each_input_port do |port|
+        task_child.each_input_port do |port|
             export port
         end
         
         provides Base::ActuatorControlledSystemSrv, :as => 'actuators'
+    end
+    
+    class Camera < Syskit::Composition
+        add Simulation::Mars, :as => "mars"
+        add Simulation::MarsCamera, :as => "task"
+        
+        task_child.each_output_port do |port|
+            export port
+        end
+        task_child.each_input_port do |port|
+            export port
+        end
+        
+        provides Base::ImageProviderSrv, :as => 'camera'
     end
 
     #TODO Add missing ones from above like the actuator
     
     class IMU < Syskit::Composition
         add Simulation::Mars, :as => "mars"
-        add Simulation::MarsIMU, :as => "imu"
+        add Simulation::MarsIMU, :as => "task"
 
-        imu_child.each_output_port do |port|
+        task_child.each_output_port do |port|
             export port
         end
 
@@ -79,6 +111,23 @@ class Simulated
         provides Base::OrientationWithZSrv, "orientation_z_samples" => "orientation_samples", :as => 'orientation_with_z'
         provides Base::ZProviderSrv, :as => 'z_samples', 'z_samples' => "pose_samples"
 
+    end
+
+    def self.getSensor(name,options = nil)
+        constants.each do |c|
+            c = eval(c)
+            if c < Syskit::Composition
+                a = c.task_child.model
+                b = name
+                if a.fullfills?(eval(b))
+                    options.each_pair do |key,value|
+                        eval("c.task_child.#{key}(#{value})")
+                    end
+                    return c
+                end
+            end
+        end
+        raise ArgumentError, "No composition found that represent an #{name} for the simulation"
     end
 end
 
