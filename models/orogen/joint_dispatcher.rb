4f39870b880259ffe12532c31389b9161e937714
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-09-17 11:26:54 +0200

    add generic configuration handling for JointDispatcher
    JointDispatcher.multiplex takes a joint dispatcher configuration
    name as input and spits a composition whose outputs are the outputs
    defined in the configuration, and the children are the input names.

---
+++ models/orogen/joint_dispatcher.rb
@@ -0,0 +1,58 @@
+require 'models/blueprints/control'
+
+module JointDispatcher
+    # Representation of a single dispatch in {Task}
+    data_service_type 'DispatchedStatusSrv' do
+        input_port 'status_in', '/base/samples/Joints'
+        output_port 'status_out', '/base/samples/Joints'
+        provides Base::JointsStatusSrv,
+            'status_samples' => 'status_out'
+    end
+
+    # Composition that represents a dispatched set of joint providers
+    #
+    # It is usually not used directly, but specialized subclasses for it are
+    # created by {multiplex} instead
+    class Dispatch < Syskit::Composition
+        add Task, :as => 'dispatcher'
+    end
+
+    def self.multiplex(*conf_names)
+        dispatches = Task.port_dispatch(*conf_names)
+
+        task = Task.specialize
+        dispatches = dispatches.map do |input, output|
+            srv = task.require_dynamic_service('dispatch', :as => "#{input}_#{output}", :status_in => input, :status_out => output)
+            [input, output, srv]
+        end
+
+        Dispatch.new_submodel :name => "JointDispatcher::Dispatcher{#{conf_names.join(",")}}" do
+            overload 'dispatcher', task
+            dispatches.each do |input, _, srv|
+                child = add Base::JointsStatusSrv, :as => input
+                child.connect_to dispatcher_child.find_data_service(srv.name)
+            end
+
+            dispatches.map { |_, output, _| output }.to_set.each do |output|
+                export dispatcher_child.find_output_port(output)
+                provides Base::JointsStatusSrv, :as => output
+            end
+        end
+    end
+
+    class Task
+        dynamic_service DispatchedStatusSrv, :as => 'dispatch' do
+            provides DispatchedStatusSrv, "status_in" => options[:status_in], "status_out" => options[:status_out]
+        end
+
+        # @return [Array<(String,String)>] the dispatches represented at the port
+        #   level, as a map from input ports to the corresponding output port
+        def self.port_dispatch(*conf_names)
+            conf = Orocos.conf.resolve('joint_dispatcher::Task', conf_names, true)
+            conf['dispatches'].map do |dispatch|
+                [dispatch['input'], dispatch['output']]
+            end
+        end
+    end
+end
+
