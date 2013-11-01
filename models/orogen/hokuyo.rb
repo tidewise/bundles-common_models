ecd7587cc0b53cabeacbcbed2ba969b5c3002a62
Thomas Roehr <thomas.roehr@dfki.de>
2013-11-01 20:23:35 +0100

    Add service interface to Hokuyo::Task

--- models/orogen/hokuyo.rb
+++ models/orogen/hokuyo.rb
@@ -4,6 +4,7 @@ require 'models/blueprints/timestamping'
 class Hokuyo::Task
     driver_for Dev::Sensors::Hokuyo, :as => 'driver'
     provides Base::TimestampInputSrv, :as => 'timestamps'
+    provides Base::LaserRangeFinderSrv, :as => 'scanner'
 
     transformer do
         associate_frame_to_ports 'laser', 'scans'
