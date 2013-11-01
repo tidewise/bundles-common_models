d18d38ac68a256a81cc3ef8c22a052de8a0de256
Thomas Roehr <thomas.roehr@dfki.de>
2013-11-01 20:24:20 +0100

    Add transform output port to ServoDynamixel::Task

--- models/orogen/servo_dynamixel.rb
+++ models/orogen/servo_dynamixel.rb
@@ -1,4 +1,9 @@
 require 'models/blueprints/devices'
 class ServoDynamixel::Task
     driver_for Dev::Actuators::Dynamixel, :as => 'driver'
+
+    transformer do
+        frames "lower", "upper"
+        transform_output "transforms", "lower" => "upper"
+    end
 end
