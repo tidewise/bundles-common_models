850c56b2c20180349bd1b6a5e612253baeafe466
Thomas Roehr <thomas.roehr@dfki.de>
2013-11-04 20:00:15 +0100

    Allow command port of dynamixel to multiplex

--- models/orogen/servo_dynamixel.rb
+++ models/orogen/servo_dynamixel.rb
@@ -2,6 +2,8 @@ require 'models/blueprints/devices'
 class ServoDynamixel::Task
     driver_for Dev::Actuators::Dynamixel, :as => 'driver'
 
+    orogen_model.find_port('command').multiplexes
+
     transformer do
         frames "lower", "upper"
         transform_output "transforms", "lower" => "upper"
