8de10034bbb161b39c27a58866f05f00ee50ea16
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-11-22 11:08:46 +0100

    fix type name

--- models/blueprints/planning.rb
+++ models/blueprints/planning.rb
@@ -3,7 +3,7 @@ import_types_from 'base'
 module Rock
     module Planning
         data_service_type 'TrajectoryExecutionSrv' do
-            input_port 'trajectory', 'std::vector</base/Trajectory'
+            input_port 'trajectory', '/std/vector</base/Trajectory>'
         end
     end
 end
