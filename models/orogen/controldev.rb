53aefc7cde31db1eaa723ea0fc7823db6804509c
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-03-26 15:42:49 +0100

    reactivated joystick

--- models/orogen/controldev.rb
+++ models/orogen/controldev.rb
@@ -6,6 +6,11 @@ module Dev
     module Controldev
 
         Base::ControlLoop.declare 'RawCommand', 'controldev/RawCommand'
+#        Base::ControlLoop.declare 'Motion2D', '/base/MotionCommand2D'
+
+        device_type 'Joystick' do
+            provides Base::Motion2DControllerSrv
+        end 
 
         device_type 'Raw' do 
             provides Base::RawCommandControllerSrv
@@ -15,6 +20,11 @@ module Dev
             provides Base::RawCommandControllerSrv
         end
     end
+end
+
+class Controldev::JoystickTask
+    driver_for Dev::Controldev::Raw, :as => 'raw'
+    driver_for Dev::Controldev::Joystick, :as => 'motion_2d'
 end
 
 class Controldev::Mouse3DTask
