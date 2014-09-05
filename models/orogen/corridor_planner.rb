db7d7cb64edb920fca652fffb18abd2c4e429bb3
Raul Dominguez <raul.dominguez@dfki.de>
2014-09-05 17:36:19 +0200

    Adaptations because CorridorPlanner::Traversability is now Traversability::Grassfire

--- models/orogen/corridor_planner.rb
+++ models/orogen/corridor_planner.rb
@@ -41,17 +41,3 @@ class CorridorPlanner::Task
     end
 end
 
-class CorridorPlanner::Traversability
-    transformer do
-        associate_frame_to_ports 'world', 'mls_map', 'traversability_map'
-    end
-
-    event :start do |context|
-        if !orocos_task.env_save_path.empty?
-            # Make sure that the directory exists
-            FileUtils.mkdir_p(File.expand_path(orocos_task.env_save_path, Roby.app.log_dir))
-        end
-
-        super(context)
-    end
-end
