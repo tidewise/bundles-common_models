fd48496c8666c269928955db796bae1e4504b7bc
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-03-25 11:18:47 -0300

    put the backward-naming-scheme flag from syskit in config/init.rb for reference

--- config/init.rb
+++ config/init.rb
@@ -6,6 +6,7 @@ Roby.scheduler = Roby::Schedulers::Temporal.new
 
 ## Uncomment to enable automatic transformer configuration support
 Syskit.conf.transformer_enabled = true
+Syskit.conf.backward_compatible_naming = true
 
 
 # Syskit.conf.redirect_local_process_server = false
