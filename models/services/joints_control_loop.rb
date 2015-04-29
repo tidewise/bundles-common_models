694c81bbe413022b81eb8d15b099e399ba29e7c4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-04-29 16:28:01 -0300

    add missing import_types_from 'base' for some services

--- models/services/joints_control_loop.rb
+++ models/services/joints_control_loop.rb
@@ -1,4 +1,6 @@
+import_types_from 'base'
 require 'rock/models/services/control_loop'
+
 Rock::Services::ControlLoop.declare \
     'Joints', '/base/commands/Joints', '/base/samples/Joints'
 
--- models/services/motion2d_control_loop.rb
+++ models/services/motion2d_control_loop.rb
@@ -1,3 +1,4 @@
+import_types_from 'base'
 require 'rock/models/services/control_loop'
 Rock::Services::ControlLoop.declare_open_loop \
     'Motion2D', '/base/commands/Motion2D'
