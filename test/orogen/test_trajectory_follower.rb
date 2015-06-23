f1f1a9d87d893e193d799cc773c6633338197dc4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-23 16:15:16 -0300

    trajectory_follower: shut up transformer-related warnings in tests

--- test/orogen/test_trajectory_follower.rb
+++ test/orogen/test_trajectory_follower.rb
@@ -3,11 +3,13 @@ using_task_library 'trajectory_follower'
 module OroGen
     module TrajectoryFollower
         describe Task do
-            it_should_be_configurable
+            it "should be configurable" do
+                syskit_stub_deploy_and_configure(Task.use_frames('world' => 'world'))
+            end
 
             it "should pass non-nil trajectory argument to the task" do
                 trajectory = [Types.base.Trajectory.new(spline: SISL::Spline3.new)]
-                task = stub_deploy_and_start(Task.with_arguments(trajectory: trajectory))
+                task = syskit_stub_deploy_configure_and_start(Task.use_frames('world' => 'world').with_arguments(trajectory: trajectory))
                 assert_equal trajectory, task.orocos_task.trajectory.read
             end
         end
