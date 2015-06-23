2c653e3752401a8a4b799d92290f98bfc595bc52
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-23 16:14:53 -0300

    test: fix path to services generated by Control.declare and ControlledSystem.declare
    They are no longer defined under ControlLoop::

--- test/services/test_control_loop.rb
+++ test/services/test_control_loop.rb
@@ -19,13 +19,13 @@ module Rock
                 end
 
                 it "properly sets up the relationship between the open loop models and the root models" do
-                    assert ControlLoop::TestOpenLoopController.provides?(ControlLoop::Controller)
-                    assert ControlLoop::TestOpenLoopControlledSystem.provides?(ControlLoop::ControlledSystem)
+                    assert TestOpenLoopController.provides?(Controller)
+                    assert TestOpenLoopControlledSystem.provides?(ControlledSystem)
                 end
                 it "creates ports that allow for an open-loop connection" do
-                    assert_equal command_type, ControlLoop::TestOpenLoopController.command_out_port.type
-                    assert_equal command_type, ControlLoop::TestOpenLoopControlledSystem.command_in_port.type
-                    ControlLoop::TestOpenLoopController.command_out_port.can_connect_to?(ControlLoop::TestOpenLoopControlledSystem.command_in_port)
+                    assert_equal command_type, TestOpenLoopController.command_out_port.type
+                    assert_equal command_type, TestOpenLoopControlledSystem.command_in_port.type
+                    TestOpenLoopController.command_out_port.can_connect_to?(TestOpenLoopControlledSystem.command_in_port)
                 end
             end
 
@@ -38,32 +38,32 @@ module Rock
                 end
 
                 it "sets up the relationship between the closed loop models and the root models" do
-                    assert ControlLoop::TestController.provides?(ControlLoop::Controller)
-                    assert ControlLoop::TestControlledSystem.provides?(ControlLoop::ControlledSystem)
+                    assert TestController.provides?(Controller)
+                    assert TestControlledSystem.provides?(ControlledSystem)
                 end
                 it "sets up the relationship between the open and closed loop models" do
-                    assert ControlLoop::TestController.provides?(ControlLoop::TestOpenLoopController)
-                    assert ControlLoop::TestControlledSystem.provides?(ControlLoop::TestOpenLoopControlledSystem)
+                    assert TestController.provides?(TestOpenLoopController)
+                    assert TestControlledSystem.provides?(TestOpenLoopControlledSystem)
                 end
                 it "leaves the open-loop models untouched" do
-                    assert !ControlLoop::TestOpenLoopController.has_port?(:status_in)
-                    assert !ControlLoop::TestOpenLoopControlledSystem.has_port?(:status_out)
+                    assert !TestOpenLoopController.has_port?(:status_in)
+                    assert !TestOpenLoopControlledSystem.has_port?(:status_out)
                 end
                 it "creates ports that allow for a feedback" do
-                    assert_equal status_type, ControlLoop::TestController.status_in_port.type
-                    assert_equal status_type, ControlLoop::TestControlledSystem.status_out_port.type
-                    ControlLoop::TestControlledSystem.status_out_port.can_connect_to?(ControlLoop::TestController.status_in_port)
+                    assert_equal status_type, TestController.status_in_port.type
+                    assert_equal status_type, TestControlledSystem.status_out_port.type
+                    TestControlledSystem.status_out_port.can_connect_to?(TestController.status_in_port)
                 end
 
                 describe "controller_for" do
                     it "returns the generated controller model" do
-                        assert_same ControlLoop::TestControlledSystem,
+                        assert_same TestControlledSystem,
                             ControlLoop.controlled_system_for('Test')
                     end
                 end
                 describe "controlled_system_for" do
                     it "returns the generated controlled system model" do
-                        assert_same ControlLoop::TestController,
+                        assert_same TestController,
                             ControlLoop.controller_for('Test')
                     end
                 end
