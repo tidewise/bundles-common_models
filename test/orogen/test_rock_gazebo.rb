2200e409044c9e864994a3839080ff7a73a9f9f2
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-26 14:14:49 -0300

    test: run rock_gazebo tests only in simulation
    The components require to be started within gazebo's process which
    we don't support right now.

--- test/orogen/test_rock_gazebo.rb
+++ test/orogen/test_rock_gazebo.rb
@@ -3,10 +3,14 @@ using_task_library 'rock_gazebo'
 module OroGen
     module RockGazebo
         describe WorldTask do
+            run_simulated
+
             it { is_configurable }
         end
 
         describe ModelTask do
+            run_simulated
+
             it { is_configurable }
         end
     end
