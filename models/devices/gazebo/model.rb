e20a8224db891293d7987df9cc82d5d96eca1ae8
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-25 14:47:59 -0300

    devices: rename the JointsControlledSystem ports to something clearer when taken from the p.o.v. of Devices::Gazebo::Model

--- models/devices/gazebo/model.rb
+++ models/devices/gazebo/model.rb
@@ -6,7 +6,14 @@ module Rock
         module Gazebo
             device_type 'Model' do
                 provides Rock::Services::Pose
-                provides Rock::Services::JointsControlledSystem
+
+                # Rename status_out and command_in to something that talks about
+                # joints
+                input_port 'joints_cmd', '/base/samples/Joints'
+                output_port 'joints_status', '/base/samples/Joints'
+                provides Rock::Services::JointsControlledSystem,
+                    'command_in' => 'joints_cmd',
+                    'status_out' => 'joints_status'
             end
         end
     end
