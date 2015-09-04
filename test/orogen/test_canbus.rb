c35f9d010bea2af79c59e3990690647bbb0f5d25
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-09-04 15:58:45 -0300

    test: make sure all tests pass, both in simulated and live mode
    Some tests are skipped because of a bug in syskit/typelib (enums not
    properly initialized when stubbing a task), but at least what is
    supposed to pass passes

--- test/orogen/test_canbus.rb
+++ test/orogen/test_canbus.rb
@@ -3,22 +3,26 @@ using_task_library 'canbus'
 module OroGen
     module Canbus
         describe Task do
-            it "fails on configure if a device has no can ID" do
-                dev = syskit_stub_device(Rock::Devices::Bus::CAN)
-                dev_task = syskit_deploy(dev)
-                bus_task = dev_task.children.first
-                assert_raises(ArgumentError) do
-                    syskit_setup(bus_task)
+            run_simulated do
+                it "fails on configure if a device has no can ID" do
+                    dev = syskit_stub_attached_device(Rock::Devices::Bus::CAN)
+                    dev.period(0.1)
+                    dev_task = syskit_deploy(dev)
+                    bus_task = dev_task.children.first
+                    assert_raises(ArgumentError) do
+                        syskit_configure(bus_task)
+                    end
                 end
-            end
-            it "calls the watch operation for each attached device" do
-                dev = syskit_stub_device(Rock::Devices::Bus::CAN)
-                dev.can_id(0x01, 0x11)
-                dev_task = syskit_deploy(dev)
-                bus_task = dev_task.children.first
-                flexmock(bus_task).should_receive_operation(:watch).once.
-                    with('dev', 0x01, 0x11)
-                syskit_setup(bus_task)
+                it "calls the watch operation for each attached device" do
+                    dev = syskit_stub_attached_device(Rock::Devices::Bus::CAN)
+                    dev.period(0.1)
+                    dev.can_id(0x01, 0x11)
+                    dev_task = syskit_deploy(dev)
+                    bus_task = dev_task.children.first
+                    flexmock(bus_task).should_receive_operation(:watch).once.
+                        with('dev', 0x01, 0x11)
+                    syskit_configure(bus_task)
+                end
             end
         end
 
--- test/orogen/test_iodrivers_base.rb
+++ test/orogen/test_iodrivers_base.rb
@@ -2,14 +2,19 @@ using_task_library 'iodrivers_base'
 
 module OroGen
     module IodriversBase
-
         describe Task do
             it { is_configurable }
         end
 
         describe Proxy do
-            it { is_configurable }
+            run_simulated do
+                it { is_configurable }
+            end
+            run_live do
+                it "fails to configure if called without a URI" do
+                    assert_raises(Orocos::StateTransitionFailed) { syskit_deploy_and_configure }
+                end
+            end
         end
-
     end
 end
