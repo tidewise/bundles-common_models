2cd21401f3b7864b978d7cd55404989411bc26b3
Thomas Roehr <thomas.roehr@dfki.de>
2016-11-02 11:11:37 +0100

    ServoDynamixel:Task provides Base::JointsControlledSystemSrv

--- models/orogen/servo_dynamixel.rb
+++ models/orogen/servo_dynamixel.rb
@@ -1,6 +1,9 @@
 require 'models/blueprints/devices'
+require 'rock/models/blueprints/control'
+
 class ServoDynamixel::Task
     driver_for Dev::Actuators::Dynamixel, :as => 'driver'
+    provides Base::JointsControlledSystemSrv, as: 'joints_controlled_system'
 
     orogen_model.find_port('command').multiplexes
 
