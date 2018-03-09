03e6299f13299c39ea6fe8248639412eabae12ad
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-03-09 15:23:05 -0300

    add a service to export gps_base/Solution (#3)

---
+++ models/services/gps.rb
@@ -0,0 +1,9 @@
+import_types_from 'gps_base'
+
+module CommonModels
+    module Services
+        data_service_type 'GPS' do
+            output_port 'solution', 'gps_base/Solution'
+        end
+    end
+end
