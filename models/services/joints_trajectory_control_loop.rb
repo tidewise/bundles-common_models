ef4ada695917e2ce6cb6e158126291a2ef0b0976
Gabriel Alcantara <alcantara@poli.ufrj.br>
2018-03-23 15:54:11 -0300

    Define the services related to joint trajectory control (#11)

---
+++ models/services/joints_trajectory_control_loop.rb
@@ -0,0 +1,5 @@
+import_types_from 'base'
+require 'common_models/models/services/control_loop'
+
+CommonModels::Services::ControlLoop.declare \
+    'JointsTrajectory', '/base/JointsTrajectory', '/base/samples/Joints'
