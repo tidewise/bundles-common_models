c6b87e9754a50ef74ef6c833bdc89cacf1fd8f53
Gabriel Alcantara <gabriel.alcantara@13robotics.com>
2018-09-21 15:36:23 -0300

    add sonar as service

---
+++ models/services/sonar.rb
@@ -0,0 +1,9 @@
+import_types_from 'base'
+
+module CommonModels
+    module Services
+        data_service_type 'Sonar' do
+            output_port 'samples', '/base/samples/Sonar'
+        end
+    end
+end
