a4d39824d40f3383ca08f29ff77effc8ec40d207
Sylvain <sylvain.joyeux@tidewise.io>
2021-06-11 12:15:33 -0300

    handle the fact that logger::Logger does not have a default deployment

--- test/orogen/test_logger.rb
+++ test/orogen/test_logger.rb
@@ -7,6 +7,10 @@ module OroGen
     logger_m = OroGen.syskit_model_by_orogen_name("logger::Logger")
 
     describe logger_m do
+        def deploy_subject_syskit_model
+            use_deployment("rock_logger").first
+        end
+
         it { is_configurable }
     end
 end
