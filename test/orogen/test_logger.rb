c7835dd5f894a9f18e09bfdd917dbbdd1882b4d1
Sylvain <sylvain.joyeux@tidewise.io>
2021-06-11 12:14:48 -0300

    fixup! workaround collision between OroGen.logger.Logger and orogen's internal logger object

--- test/orogen/test_logger.rb
+++ test/orogen/test_logger.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 using_task_library 'logger'
 
-# OroGen.logger already exists, we can't access the model for logger::Logger
-# using the method syntax
-logger_m = OroGen.syskit_model_by_orogen_name("logger::Logger")
-
 module OroGen
+    # OroGen.logger already exists, we can't access the model for logger::Logger
+    # using the method syntax
+    logger_m = OroGen.syskit_model_by_orogen_name("logger::Logger")
+
     describe logger_m do
         it { is_configurable }
     end
