d7d2505978401fb34e26395a65225de4debb390f
MateusPedrosa <mateus.pedrosa@tidewise.io>
2021-06-17 14:57:58 -0300

    Implement tests for log rotation

--- test/orogen/test_logger.rb
+++ test/orogen/test_logger.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-using_task_library 'logger'
+
+using_task_library "logger"
 
 module OroGen
     # OroGen.logger already exists, we can't access the model for logger::Logger
@@ -12,5 +13,47 @@ module OroGen
         end
 
         it { is_configurable }
+
+        describe "log rotation" do
+            it "increments index in log file name after rotation" do
+                task = syskit_stub_deploy_and_configure(logger_m)
+                task.orocos_task.property("overwrite_existing_files").write(false)
+                task.orocos_task.property("auto_timestamp_files").write(false)
+
+                assert_empty(task.orocos_task.property("file").read)
+
+                task.rotate_log
+
+                assert_equal(
+                    "#{task.orocos_task.basename}.0.log",
+                    task.orocos_task.property("file").read
+                )
+
+                task.rotate_log
+
+                assert_equal(
+                    "#{task.orocos_task.basename}.1.log",
+                    task.orocos_task.property("file").read
+                )
+            end
+
+            it "returns previous file after rotation" do
+                task = syskit_stub_deploy_and_configure(logger_m)
+                task.orocos_task.property("overwrite_existing_files").write(false)
+                task.orocos_task.property("auto_timestamp_files").write(false)
+
+                assert_equal [""], task.rotate_log
+
+                assert_equal(
+                    ["#{task.orocos_task.basename}.0.log"],
+                    task.rotate_log
+                )
+
+                assert_equal(
+                    ["#{task.orocos_task.basename}.1.log"],
+                    task.rotate_log
+                )
+            end
+        end
     end
 end
